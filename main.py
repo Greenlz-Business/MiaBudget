aa1ebb7eae5e2779d580ee8ce3b510b1491b9582
GreenLz <blazejm123@gmail.com>
2024-11-20 23:28:09 +0000

    Added requirements and started main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+# File created by GreenLz
+# MiaBudget was made from a quick spaghetti script called Closed-GLZ-Bugdet
+# It is a saucepan full of Youtube tutorials, Chatgpt poop, great forums and own intuition
+# It helped me to learn python and github environment with version tracking
+# Project started on 19-Nov-2024
+
+exec(open(R"C:\Users\WE FIX IT\Desktop\MiaBudget\converter\transaction_processor.py").read())
