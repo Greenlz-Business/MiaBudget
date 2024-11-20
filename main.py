e9bcb3d82e732f6fcb860346f5876bfab4977250
Greenlz-Business <greenlz.business@gmail.com>
2024-11-20 23:51:28 +0000

    Fixed git user

--- main.py
+++ main.py
@@ -3,5 +3,6 @@
 # It is a saucepan full of Youtube tutorials, Chatgpt poop, great forums and own intuition
 # It helped me to learn python and github environment with version tracking
 # Project started on 19-Nov-2024
+__version__ = "0.1.0"
 
 exec(open(R"C:\Users\WE FIX IT\Desktop\MiaBudget\converter\transaction_processor.py").read())
